b0026f3eb2945bda7d1368556b89c1298a6ee536
TinoOuwerkerk <tino.ouwerkerk@gmail.com>
2023-03-20 17:15:09 +0100

    exercise 3,4

--- assignment2.r
+++ assignment2.r
@@ -2,6 +2,9 @@
 library(igraph)
 library(RColorBrewer)
 library(visNetwork)
+library(ggplot2)
+library(tidyverse)
+library(ggpubr)
 setwd("C:/Users/tinot/uu/human_network_lab_2")
 #datainput
 highschool_edge<-read.csv("Highschool_network_edge.csv",header=FALSE)
@@ -30,11 +33,17 @@ facebook_edges<-data.frame(from=c(as.character(facebook_edge[,1])),
 Facebook<-graph_from_data_frame(facebook_edges,directed = FALSE,vertices = facebook_nodes)
 summary(Facebook)
 
+# Question 1
 #function to calculate centrality metrics
-which.max(degree(Highschool, mode = "all"))
-which.max(closeness(Highschool, normalized = TRUE))
-which.max(betweenness(Highschool, directed = FALSE, normalized = TRUE))
-which.max(eigen_centrality(Highschool)$vector)
+degree_n = (degree(Highschool, mode = "all"))
+closeness_n = (closeness(Highschool, normalized = TRUE))
+betweenness_n = betweenness(Highschool, directed = FALSE, normalized = TRUE)
+eigen_n = eigen_centrality(Highschool)
+
+which.max(degree_n)
+which.max(closeness_n)
+which.max(betweenness_n)
+which.max(eigen_n$vector)
 #function to visualize the network (with interaction)
 set.seed(100)
 Highschool_interactive_layout<-visNetwork(data.frame(id=V(Highschool)$name),
@@ -44,3 +53,18 @@ ties") %>%
   visNodes(shape="circle",label = TRUE) %>%
   visOptions(highlightNearest = list(enabled = T, hover = T), nodesIdSelection = T)
 Highschool_interactive_layout
+
+# Question 2
+# Correlation High school network
+ggplot() + geom_point(aes(degree_n, betweenness_n)) + geom_smooth(aes(degree_n, betweenness_n), method="lm") + stat_cor(aes(degree_n, betweenness_n), method = "pearson")
+ggplot() + geom_point(aes(degree_n, closeness_n)) + geom_smooth(aes(degree_n, closeness_n), method="lm") + stat_cor(aes(degree_n, closeness_n), method = "pearson")
+ggplot() + geom_point(aes(degree_n, eigen_n$vector)) + geom_smooth(aes(degree_n, eigen_n$vector), method="lm") + stat_cor(aes(degree_n, eigen_n$vector), method = "pearson")
+
+# Correlation Facebook network
+degree_n_fb = (degree(Facebook, mode = "all"))
+closeness_n_fb = (closeness(Facebook, normalized = TRUE))
+betweenness_n_fb = betweenness(Facebook, directed = FALSE, normalized = TRUE)
+eigen_n_fb = eigen_centrality(Facebook)
+ggplot() + geom_point(aes(degree_n_fb, betweenness_n_fb)) + geom_smooth(aes(degree_n_fb, betweenness_n_fb), method="lm") + stat_cor(aes(degree_n_fb, betweenness_n_fb), method = "pearson")
+ggplot() + geom_point(aes(degree_n_fb, closeness_n_fb)) + geom_smooth(aes(degree_n_fb, closeness_n_fb), method="lm") + stat_cor(aes(degree_n_fb, closeness_n_fb), method = "pearson")
+ggplot() + geom_point(aes(degree_n_fb, eigen_n_fb$vector)) + geom_smooth(aes(degree_n_fb, eigen_n_fb$vector), method="lm") + stat_cor(aes(degree_n_fb, eigen_n_fb$vector), method = "pearson")
